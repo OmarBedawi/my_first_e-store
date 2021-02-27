fb451736e40be74e284d9542bd1c20d9b67265b7
OmarBedawi <omarbedawi@hotmail.com>
2021-02-27 19:51:29 +0000

    customized admin, ebook images renamed

--- ebooks/admin.py
+++ ebooks/admin.py
@@ -2,5 +2,29 @@ from django.contrib import admin
 from .models import Ebook, Category
 
 # Register your models here.
-admin.site.register(Ebook)
-admin.site.register(Category)
+
+
+class EbookAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'title',
+        'category',
+        'authors',
+        'year',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Ebook, EbookAdmin)
+admin.site.register(Category, CategoryAdmin)
