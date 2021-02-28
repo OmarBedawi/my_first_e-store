df27ac932ed283d7258256a6960109b97907c3ea
OmarBedawi <omarbedawi@hotmail.com>
2021-02-28 16:54:31 +0000

    added ebooks views and templates

--- ebooks/views.py
+++ ebooks/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Ebook
 
 # Create your views here.
+
+
+def all_ebooks(request):
+    """ A view to show all ebooks, including sorting and search queries """
+
+    ebooks = Ebook.objects.all()
+
+    context = {
+        'ebooks': ebooks,  # pylint: disable=no-member
+    }
+
+    return render(request, 'ebooks/ebooks.html', context)
