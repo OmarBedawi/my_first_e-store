dc2170905569da6efadc2888c3da9869fc33ff31
OmarBedawi <omarbedawi@hotmail.com>
2021-03-11 10:06:11 +0000

    deployment part 2

--- my_estore/settings.py
+++ my_estore/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = ')co_aaz^t91)j&e(z16b)b-^%(mqj=_u=6c+8y*oe(*gp88$%g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['omar-e-book-store.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -120,12 +120,17 @@ WSGI_APPLICATION = 'my_estore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
- }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
