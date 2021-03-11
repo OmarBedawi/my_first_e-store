d647a818f4a0892fbba7f58e446ecfae0767a4cd
OmarBedawi <omarbedawi@hotmail.com>
2021-03-11 10:25:42 +0000

    removed secret key and set debug

--- my_estore/settings.py
+++ my_estore/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')co_aaz^t91)j&e(z16b)b-^%(mqj=_u=6c+8y*oe(*gp88$%g'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['omar-e-book-store.herokuapp.com', 'localhost']
 
