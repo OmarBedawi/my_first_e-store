a349c254987b151ecdc5f963ca43f68af9086363
OmarBedawi <omarbedawi@hotmail.com>
2021-04-05 22:08:58 +0000

    added to 'add to bag' and context, the functionality for ebook_readers

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,18 +1,22 @@
 from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
-from ebooks.models import Ebook
+from ebooks.models import Ebook, Ebook_reader
 
 
 def bag_contents(request):
 
+    print("this is the one")
     bag_items = []
     total = 0
     product_count = 0
     bag = request.session.get('bag', {})
 
     for item_id, quantity in bag.items():
-        ebook = get_object_or_404(Ebook, pk=item_id)
+        try:
+            ebook = get_object_or_404(Ebook, pk=item_id)
+        except:
+            ebook = get_object_or_404(Ebook_reader, pk=item_id)
         total += quantity * ebook.price
         product_count += quantity
         bag_items.append({
--- bag/views.py
+++ bag/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import (
     render, redirect, reverse, HttpResponse, get_object_or_404)
 from django.contrib import messages
 
-from ebooks.models import Ebook
+from ebooks.models import Ebook, Ebook_reader
 
 # Create your views here.
 
@@ -15,19 +15,29 @@ def view_bag(request):
 
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
-
-    ebook = get_object_or_404(Ebook, pk=item_id)
+    
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
-
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
-        messages.success(
+    try:
+        ebook = get_object_or_404(Ebook, pk=item_id)
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+            messages.success(
             request, f'Updated  "{ebook.title}" quantity to {bag[item_id]}')
-    else:
-        bag[item_id] = quantity
-        messages.success(request, f'Added "{ebook.title}" to your bag')
+        else:
+            bag[item_id] = quantity
+            messages.success(request, f'Added "{ebook.title}" to your bag')
+    except:
+        ebook = get_object_or_404(Ebook_reader, pk=item_id)
+
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+            messages.success(
+                request, f'Updated  "{ebook.model}" quantity to {bag[item_id]}')
+        else:
+            bag[item_id] = quantity
+            messages.success(request, f'Added "{ebook.model}" to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
